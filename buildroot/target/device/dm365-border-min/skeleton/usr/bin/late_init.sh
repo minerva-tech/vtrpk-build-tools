41c10457467ffa6318a4504bb59d70e910242f1e
Andrey Sivchikov <sivand@mail.ru>
2014-09-14 19:53:41 +0700

    build spi eeprom driver as module

--- buildroot/target/device/dm365-border-min/skeleton/usr/bin/late_init.sh
+++ buildroot/target/device/dm365-border-min/skeleton/usr/bin/late_init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sleep 2
+sleep 1
+
+/sbin/modprobe mtdblock && /sbin/modprobe davinci_nand && /sbin/modprobe m25p80 && /sbin/modprobe at25 && /sbin/modprobe davinci_spi
 
 /sbin/modprobe davinci && /sbin/modprobe g_cdc && /sbin/ifup -a
-
-/sbin/modprobe mtdblock && /sbin/modprobe && /sbin/modprobe davinci_nand && /sbin/modprobe m25p80 && /sbin/modprobe davinci_spi
 
 /etc/init.d/S50dropbear start
