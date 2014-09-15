321c3254ea07155be86b32e12a13578760da1ba4
laptop hedgehog <sivand@mail.ru>
2014-09-15 15:39:59 +0700

    make-up

--- buildroot/rebuild.sh
+++ buildroot/rebuild.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+make || exit 1
+
 (cd output/build/linux-custom && rm -f .stamp_built  .stamp_images_installed  .stamp_initramfs_rebuilt .stamp_target_installed)
 
-make || exit 1
+make || exit 2
 
