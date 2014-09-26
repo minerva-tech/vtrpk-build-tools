ff32668c4fc65ff73418d3b054e2076193480ce3
Andrey Sivchikov <sivand@mail.ru>
2014-09-26 09:15:45 +0700

    minimize boot time

--- buildroot/target/device/dm365-border-min/skeleton/usr/bin/late_init.sh
+++ buildroot/target/device/dm365-border-min/skeleton/usr/bin/late_init.sh
@@ -2,8 +2,14 @@
 
 sleep 2
 
+/sbin/modprobe mtdblock
+
+/sbin/modprobe m25p80
+
 /sbin/modprobe davinci_nand
 
 /sbin/modprobe davinci && /sbin/modprobe g_cdc && /sbin/ifup -a
+
+/usr/sbin/udhcpd
 
 /etc/init.d/S50dropbear start
