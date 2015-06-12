ea15d3b10ef85f240484cdf13e89e395b7a6a823
minerva-tech <kovliagin@minerva-tech.com>
2015-06-12 12:30:24 +0600

    switch to CDC RNDIS driver

--- buildroot/target/device/dm365-border-min/skeleton/usr/bin/late_init.sh
+++ buildroot/target/device/dm365-border-min/skeleton/usr/bin/late_init.sh
@@ -8,7 +8,7 @@ sleep 2
 
 /sbin/modprobe davinci_nand
 
-/sbin/modprobe davinci && /sbin/modprobe g_cdc && /sbin/ifup -a
+/sbin/modprobe davinci && /sbin/modprobe g_ether && /sbin/ifup -a
 
 /usr/sbin/udhcpd
 
