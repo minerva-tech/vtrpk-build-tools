b20e9b4e0c56b815502857d562b71ed7786f7090
Andrey Sivchikov <sivand@mail.ru>
2015-08-02 11:15:59 +0600

    add platform minerva-sdmmc

---
+++ buildroot/target/device/dm365-vrtpk/skeleton-sdmmc/usr/bin/late_init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sleep 2
+
+/sbin/modprobe mtdblock
+
+/sbin/modprobe m25p80
+
+/sbin/modprobe davinci_nand
+
+/sbin/modprobe davinci && /sbin/modprobe g_ether && /sbin/ifup -a
+
+/usr/sbin/udhcpd
+
+/etc/init.d/S50dropbear start
