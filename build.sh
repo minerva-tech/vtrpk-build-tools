0b357bd88af9ae2d4d6248846fbbdcf6b92ea5d4
Andrey Sivchikov <sivand@mail.ru>
2015-08-02 17:50:39 +0600

    update building

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+for i in x-tools bootloader
+do
+    cd $i
+    if [ ! -f .built]; then
+	./build.sh || exit 1
+	touch .built
+    fi
+    cd ../
+done
+
 buildroot ()
 {
     [ -f Output/${1}/.built ] && return
@@ -11,11 +21,23 @@ buildroot ()
 	done
     fi
     pushd Output/$1
+    if [ -n "$2" -a -d ../${2}/output/host ]; then
+	mkdir -p output
+	cd output
+	for i in host staging toolchain
+	do
+	    ln -s ../../${2}/output/${i} ${i}
+	done
+	cp -a ../../${2}/output/stamps stamps
+	cd ../
+    fi
     cp configs/${1}_defconfig .config
-    make oldconfig
+    make oldconfig || exit 1
     make || exit 1
     popd
     touch Output/${1}/.built
 }
 
 buildroot minerva_sdmmc
+buildroot vrtpk
+buildroot vrtvk
