191bd57eef5d13cac055194a2db68134b00ed742
minerva-tech <kovliagin@minerva-tech.com>
2015-11-11 12:16:56 +0600

    set write protect pin to high

--- buildroot/package/mongoose/fwupdate.sh
+++ buildroot/package/mongoose/fwupdate.sh
@@ -20,6 +20,14 @@ rm -f ${LOG}
 
 log "Writing firmware ..."
 
+/bin/echo 81 > /sys/class/gpio/export
+/bin/sleep 1
+/bin/echo out > /sys/class/gpio/gpio81/direction
+/bin/echo 1 > /sys/class/gpio/gpio81/value
+
 /usr/bin/fw_write -l kernel -f $1 -a 80008000 -m a1aced66 -v | tee -a ${LOG} || exit_error "FAILED" 1
 
 /sbin/rmmod davinci_nand
+
+/bin/echo 0 > /sys/class/gpio/gpio81/value
+/bin/echo 81 > /sys/class/gpio/unexport
