e9652a052e91371b2f06efdf46d3ebf4d0503b14
Andrey Sivchikov <sivand@mail.ru>
2015-11-13 19:33:45 +0600

    update late init script from vrtpk rootfs

--- buildroot/target/device/dm365-vrtpk/skeleton-vrtvk/usr/bin/late_init.sh
+++ buildroot/target/device/dm365-vrtpk/skeleton-vrtvk/usr/bin/late_init.sh
@@ -8,8 +8,10 @@ sleep 2
 
 #/sbin/modprobe davinci_nand
 
-/sbin/modprobe davinci && /sbin/modprobe g_ether && /sbin/ifup -a
+/sbin/modprobe davinci || exit 1
 
-/usr/sbin/udhcpd
+/sbin/modprobe -a nls_utf8 nls_ascii nls_base nls_cp855 nls_cp866 nls_iso8859-1 nls_iso8859-5 nls_koi8-r vfat
 
-/etc/init.d/S50dropbear start
+/sbin/modprobe -a sd_mod usb-storage
+
+/sbin/modprobe g_ether && /sbin/ifup -a && /usr/sbin/udhcpd && /etc/init.d/S50dropbear start
