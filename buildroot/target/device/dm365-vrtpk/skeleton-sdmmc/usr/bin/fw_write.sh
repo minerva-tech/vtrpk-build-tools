cf8b1bf6d02e0694c328dac926d00b1bfaf97648
Andrey Sivchikov <sivand@mail.ru>
2015-11-13 19:40:34 +0600

    sdmmc: support of NAND Write-Protection pin

--- buildroot/target/device/dm365-vrtpk/skeleton-sdmmc/usr/bin/fw_write.sh
+++ buildroot/target/device/dm365-vrtpk/skeleton-sdmmc/usr/bin/fw_write.sh
@@ -10,6 +10,11 @@ sha1sum Image.sha1 || exit 3
 [ ! -f ubl_nand.bin ] && exit 4
 sha1sum ubl_nand.bin.sha1 || exit 5
 
+/bin/echo 81 > /sys/class/gpio/export
+/bin/sleep 1
+/bin/echo out > /sys/class/gpio/gpio81/direction
+/bin/echo 1 > /sys/class/gpio/gpio81/value
+
 /usr/bin/fw_write -l bootloader -f ubl_nand.bin -v -a 20 -m a1aced00 || exit 6
 /usr/bin/fw_write -l rescue -f Image -v -a 80008000 -m a1aced66 || exit 7
 
@@ -18,3 +23,6 @@ umount /mnt/1 || exit 8
 
 cd /sys/class/leds/error
 cat max_brightness > brightness
+
+/bin/echo 0 > /sys/class/gpio/gpio81/value
+/bin/echo 81 > /sys/class/gpio/unexport
