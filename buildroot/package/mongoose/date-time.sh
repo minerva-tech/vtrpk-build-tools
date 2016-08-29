d0d5fbc29d1f8369c8df0c90b1cfa4d7f0eebc4a
Andrey Sivchikov <sivand@mail.ru>
2016-08-29 10:15:00 +0700

    finalize date-time.sh

--- buildroot/package/mongoose/date-time.sh
+++ buildroot/package/mongoose/date-time.sh
@@ -17,3 +17,13 @@ exit_error()
 rm -f ${LOG}
 
 log "Setting ${1}.${2}.${3} ${4}:${5}:${6} ..."
+date -s ${3}.${2}.${1}-${4}:${5}:${6} || exit_error "Failed to set system time & date" 1
+
+log "Synchronizing rtc with system timer ..."
+hwclock -w -f /dev/rtc0 || exit_error "Failed to syncronize " 2
+
+log "Waiting for 2 secons ..."
+sleep 2
+
+log "Reading rtc ..."
+hwclock -r /dev/rtc0 >> ${LOG} || exit_error "Failed to read rtc" 3
